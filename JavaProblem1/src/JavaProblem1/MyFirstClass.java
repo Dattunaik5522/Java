438030a9f15bf9f37997167199c99db5970cf9ed
Dattunaik5252 <gugulothdattu000@gmail.com>
2022-05-03 17:28:24 +0530

    created Classa and object

--- JavaProblem1/src/JavaProblem1/MyFirstClass.java
+++ JavaProblem1/src/JavaProblem1/MyFirstClass.java
@@ -1,9 +1,25 @@
 package JavaProblem1;
 
+class First {
+	int id;
+	String name;
+	
+}
+
 public class MyFirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		First object1 = new First();
+		First object2 = new First();
+		object1.id = 1;
+		object1.name = "DattuNaik G";
+		object2.id =  2;
+		object2.name = "DurgaNaik G";
+		System.out.println(object1.id);
+		System.out.println(object1.name);
+		System.out.println(object2.id);
+		System.out.println(object2.name);
 		System.out.println("Hello Eclipse");
 
 	}
